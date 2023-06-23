a2ccf4aa7b5906caf60b101c3ff6a09f4738038d
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2023-06-23 02:10:57 +0200

    Initial Commit

---
+++ Speedometer/SpeedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedometerApp.swift
+//  Speedometer
+//
+//  Created by user242911 on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SpeedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
