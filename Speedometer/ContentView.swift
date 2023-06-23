1effae0b312d6f430b1a2bc044e30a188c0d147f
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2023-06-23 02:54:32 +0200

    Init commit

--- Speedometer/ContentView.swift
+++ Speedometer/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var speed = 611
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            ForEach(0..<digitCount(), id: \.self) { i in
+                Text(String(getSpeedDigit(digitIdx: i)))
+                    .font(.largeTitle)
+            }
         }
         .padding()
+    }
+    
+    func getSpeedDigit(digitIdx: Int32) -> Int32 {
+        return Int32(Double(speed) / pow(Double(10), Double((digitCount() - 1) - digitIdx))) % 10
+    }
+    
+    func digitCount() -> Int32 {
+        return Int32(log10(Double(speed))) + 1
     }
 }
 
