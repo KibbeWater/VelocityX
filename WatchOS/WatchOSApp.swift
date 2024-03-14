4a6a44d33858d3f528e3de3c892e2bd361a80f77
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-03-14 14:25:11 +0100

    Update Logo

---
+++ WatchOS/WatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSApp.swift
+//  WatchOS Watch App
+//
+//  Created by user242911 on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
